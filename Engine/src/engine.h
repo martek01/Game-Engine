747bc8f8d70b02bc2779d28a2b05dd94bb17cd7a
Martek01 <martek01@gmx.de>
2014-08-29 16:31:35 +0200

    Added missing engine.h

---
+++ Engine/src/engine.h
@@ -0,0 +1,15 @@
+//
+// engine.h
+// Engine
+//
+// Martek01 - 28.08.2014
+//
+
+#ifndef ENGINE_ENGINE_H
+#define ENGINE_ENGINE_H
+
+#include "scene.h"
+#include "scenenode2d.h"
+#include "scenenode3d.h"
+
+#endif /* ENGINE_ENGINE_H */
